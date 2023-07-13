a0e1cbea91d0b8b41c0aa3a03509b07ae5989e81
MuDavid <139471511+MuDavid@users.noreply.github.com>
2023-07-13 19:51:30 +0400

    Add files via upload

---
+++ Homework.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int bitcheck(int number){
+
+    int new = (number << 4) & 1;
+
+    if (new == 0){
+
+    number |=(1<<4);    
+    }
+
+    return number;
+
+}
+
+int main(){
+int a;
+
+
+scanf("%d", &a);
+
+int result = bitcheck(a);
+
+
+    if (a==result){
+
+        printf("%d", a);
+
+    }
+    else{printf("%d",result);}
+
+    return 0; 
+
+
+
+}
